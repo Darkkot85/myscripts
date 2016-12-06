f729406f58b9e2d017057efbddb34f5b378a5158
Darkkot <smolinkot85@gmail.com>
2016-12-06 12:33:16 +0800

    Complete

--- resize_and_rename.py
+++ resize_and_rename.py
@@ -12,9 +12,10 @@ for i in range(len(listOfImage)):
     img = Image.open(listOfImage[i])
     ratio = (width / float(img.size[0]))
     height = int((float(img.size[1]) * float(ratio)))
-    img.resize((width, height), Image.ANTIALIAS)
-    img.save(listOfImage[i])
-    print(i)
+    image_resized =  img.resize((width, height), Image.ANTIALIAS)
+    image_resized.save(listOfImage[i])
+    os.renames(listOfImage[i], str(i+1) + '.jpg')
+
 
 
 
