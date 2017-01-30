4abecbe896fff5f895e9d086b72e9ee32e94fc58
Darkkot85 <smolinkot85@yandex.ru>
2017-01-30 16:37:00 +0800

    Итог

--- scraper.py
+++ scraper.py
@@ -1,7 +1,24 @@
+#http://baguzin.ru/wp/?p=14420
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
 
-html =urlopen("http://csn.irk.ru")
-bsObj = BeautifulSoup(html.read())
-print(bsObj.p)
+
+try:
+    html = urlopen("https://www.python.org")
+except HTTPError as e:
+    print(e)
+try:
+    bsObj = BeautifulSoup(html, "html.parser")
+    news = bsObj.find('div', {"class": "event-widget"}).ul.findAll("li")
+    if news:
+        for li in news:
+            print('{0:10} ==> {1:10}'.format(li.time.get_text(), li.a.get_text()))
+    else:
+        print("Блок с событиями не найден, либо изменилась разметка страницы")
+except AttributeError as e:
+    print("Произошла ошибка парсинга")
+
+
+
